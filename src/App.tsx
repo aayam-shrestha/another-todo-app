0f9b6baad5ada6cbaefd0ef38c38f2b117fe3e7e
aayam-shrestha <aayam2071@gmail.com>
2023-04-03 12:51:24 -0400

    added functions to perform calculations

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,76 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./styles.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [calc, setCalc] = useState("");
+  const [result, setResult] = useState("");
+
+  const operators = ["/", "*", "+", "-", "."];
+
+  const updateCalc = (value: string) => {
+    if (
+      (operators.includes(value) && calc === "") ||
+      (operators.includes(value) && operators.includes(calc.slice(-1)))
+    )
+      return;
+    setCalc(calc + value);
+
+    if (!operators.includes(value)) {
+      setResult(eval(calc + value).toString());
+    }
+  };
+
+  const calculate = () => {
+    setCalc(eval(calc).toString());
+  };
+
+  const deleteLast = () => {
+    if (calc == "") {
+      return;
+    }
+    const value = calc.slice(0, -1);
+    setCalc(value);
+  };
+
+  const clearAll = () => {
+    setCalc("");
+  };
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="calculator-grid">
+      <div className="output">
+        <div className="result">{result ? `(${result})` : ""}</div>
+        <div className="current-operand">{calc || 0}</div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <button
+        onClick={() => {
+          clearAll();
+        }}
+        className="span-two"
+      >
+        AC
+      </button>
+      <button onClick={() => deleteLast()}>DEL</button>
+      <button onClick={() => updateCalc("/")}>/</button>
+      <button onClick={() => updateCalc("1")}>1</button>
+      <button onClick={() => updateCalc("2")}>2</button>
+      <button onClick={() => updateCalc("3")}>3</button>
+      <button onClick={() => updateCalc("*")}>*</button>
+      <button onClick={() => updateCalc("4")}>4</button>
+      <button onClick={() => updateCalc("5")}>5</button>
+      <button onClick={() => updateCalc("6")}>6</button>
+      <button onClick={() => updateCalc("+")}>+</button>
+      <button onClick={() => updateCalc("7")}>7</button>
+      <button onClick={() => updateCalc("8")}>8</button>
+      <button onClick={() => updateCalc("9")}>9</button>
+      <button onClick={() => updateCalc("-")}>-</button>
+      <button onClick={() => updateCalc(".")}>.</button>
+      <button onClick={() => updateCalc("0")}>0</button>
+      <button className="span-two" onClick={() => calculate()}>
+        =
+      </button>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
